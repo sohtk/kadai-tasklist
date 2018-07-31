72b1331a6c93c88d594c9a3f5706dccdfa6b06f1
soh.tk <open.sohtk2956@gmail.com>
2018-07-31 21:49:19 +0900

    Add getAllMessages

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,10 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table (name = "tasks")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTasks",
+            query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+            )
+})
+@Table(name = "tasks")
 
 public class Task {
     @Id
@@ -27,7 +35,7 @@ public class Task {
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
-    @Column(name = "update_at", nullable = false)
+    @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
     public Integer getId(){
